a314b088545292701502d7d0f744ae6bfe256e24
Jrb <jrb.youssef@gmail.com>
2021-12-12 12:59:45 +0100

    Small code refactor

--- src/server.js
+++ src/server.js
@@ -2,28 +2,34 @@ require('dotenv').config({ path: '../.env' });
 require('module-alias/register');
 
 const path = require('path');
+const cors = require('cors');
+
 const express = require('express');
 const app = express();
-const cors = require('cors');
-app.use(cors())
 
 const routes = require('./routes')
+
+app.use(cors())
 
 /** Without this 2 middlewares req.body is undefined */
 app.use(express.json());
 
-app.use('/api', routes)
 
-// static file for images
+
+/** Static storage folder for images */
 const dir = path.join(process.env.PWD, process.env.STORAGE_FOLDER);
 app.use(express.static(dir));
+
+/** Main routes */
+app.use('/api', routes)
 
 /**** Home page (TEST) ****/
 app.get('/', (req, res) => {
   res.json({ success: req.body });
 });
 
-const PORT = process.env.API_PORT || 5000
+const PORT = process.env.API_PORT
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
